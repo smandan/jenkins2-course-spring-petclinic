bc776adab5bdd166477a0dba9777cca7bf8391fe
Suresh Mandan <mandas1@nationwide.com>
2020-09-13 23:49:34 -0400

    testing for failure

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -39,11 +39,11 @@ public class ValidatorTests {
         assertThat(constraintViolations.size()).isEqualTo(1);
         ConstraintViolation<Person> violation = constraintViolations.iterator().next();
 
-/**
         assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
-*/
 
+/**
         assertThat(violation.getPropertyPath().toString()).isEqualTo("abcd");
+*/
 
         assertThat(violation.getMessage()).isEqualTo("may not be empty");
     }
